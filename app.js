aa159f7b45e839f67ea9b26b085e125b0bfb3e72
bass9030 <bass90301.naver.comm@gmail.com>
2024-09-11 22:07:00 +0900

    env 중복 제거

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+if(app.get('env') == 'development')
+    app.use('/api/initTable', require('./routes/api/_initDB'));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
