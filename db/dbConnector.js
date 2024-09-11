1e25b06a43042074b901668139eeabef0c211b18
bass9030 <bass90301.naver.comm@gmail.com>
2024-09-11 22:06:42 +0900

    env 중복 제거

--- db/dbConnector.js
+++ db/dbConnector.js
@@ -1,10 +1,10 @@
 const MariaDB = require('mariadb');
-require('dotenv').config();
 
 const db = MariaDB.createPool({
     host: process.env.DB_HOST,
     port: 3306,
     database: process.env.DB_NAME,
+    user: process.env.DB_USER,
     password: process.env.DB_PASS,
 });
 
