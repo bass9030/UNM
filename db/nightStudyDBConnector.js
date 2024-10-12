0a2a547ecd08135ae226ccbe785bfdad393405bd
bass9030 <bass90301.naver.comm@gmail.com>
2024-10-12 08:49:31 +0000

    auth DB와 분리 대응

--- db/nightStudyDBConnector.js
+++ db/nightStudyDBConnector.js
@@ -3,7 +3,7 @@ const MariaDB = require('mariadb');
 const db = MariaDB.createPool({
     host: process.env.DB_HOST,
     port: 3306,
-    database: process.env.DB_PREFIX + '_nightStudyInfo',
+    database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
 });
