511779e279aefdbe4cd2e129d4a95427839e0678
bass9030 <bass90301.naver.comm@gmail.com>
2024-10-12 10:50:37 +0000

    출석 페이지 권한 체크 추가

--- routes/checkout.js
+++ routes/checkout.js
@@ -1,10 +1,18 @@
 var express = require("express");
 var router = express.Router();
-const manager = require("../db/nightStudyManager.js");
+var createError = require("http-errors");
 
 /* GET home page. */
 router.get("/", async function (req, res, next) {
-  res.render("checkout");
+    console.log(req.jwt);
+    if (req.jwt.role !== 2) {
+        res.status(403);
+        return res.json({
+            success: false,
+            message: "만료되었거나 올바르지 않은 토큰입니다.",
+        });
+    }
+    res.render("checkout");
 });
 
 module.exports = router;
