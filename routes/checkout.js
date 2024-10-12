50422a49cf9b0de96142dcedbfb948eecbd9d93a
bass9030 <bass90301.naver.comm@gmail.com>
2024-10-12 08:52:01 +0000

    출석체크용 페이지 추가(바코드 인식 모듈 테스트용)

---
+++ routes/checkout.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+const manager = require("../db/nightStudyManager.js");
+
+/* GET home page. */
+router.get("/", async function (req, res, next) {
+  res.render("checkout");
+});
+
+module.exports = router;
