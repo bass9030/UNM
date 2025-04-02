4054af807a88597388131f3a2d0318f5c8fb4a4c
bass9030 <bass90301.naver.comm@gmail.com>
2025-04-03 01:40:41 +0900

    환경 변수명 수정

--- db/nightStudyDBConnector.js
+++ db/nightStudyDBConnector.js
@@ -1,15 +1,15 @@
-const MariaDB = require('mariadb');
+const MariaDB = require("mariadb");
 
 const db = MariaDB.createPool({
     host: process.env.DB_HOST,
     port: 3306,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
 });
 
 module.exports = {
     getConnection: async () => {
         return await db.getConnection();
-    }
-}
+    },
+};
